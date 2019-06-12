8862df05f3631ea7297337e97f1dd6a26106d3a8
huanshenyi <txy1226052@gmail.com>
2019-06-12 23:22:38 +0900

    流れは通った

--- features/register.feature
+++ features/register.feature
@@ -3,14 +3,14 @@ Feature: Register User
      As a developer
      This is my first bdd project
      Scenario: open register website
-         When I open the register website
+         When I open the register website "http://www.5itest.cn/register"
          Then I expect that the title is "注册"
 
 
-#     Scenario: input username
-#       When I set with useremail "mushishid1@qq.com"
-#       And I set with username "mushishid1"
-#       And I set with password "mushish"
-#       And I set with code "resd"
-#       And I click with registerbutton
-#       Then I expect that text "验证码错误"
+     Scenario: input username
+         When I set with useremail "mushishid1@qq.com"
+         And I set with username "mushishid1"
+         And I set with password "mushish"
+         And I set with code "resd"
+         And I click with registerbutton
+         Then I expect that text "验证码错误"
--- features/steps/register_user.py
+++ features/steps/register_user.py
@@ -1,12 +1,38 @@
 from behave import *
 
 use_step_matcher("re")
-@when('I open the register website')
-def step_register(context):
-    context.driver.get("http://www.5itest.cn/register")
+@when('I open the register website "([^"]*)"')
+def step_register(context, url):
+    # context.driver.get("http://www.5itest.cn/register")
+    context.driver.get(url)
 
 
 @then('I expect that the title is "([^"]*)"')
 def step_register(context, title_name):
     title = context.driver.title
     assert title_name in title
+
+@when('I set with useremail "([^"]*)"')
+def step_register(context,useremail):
+    context.driver.find_element_by_id("register_email").send_keys(useremail)
+
+@when('I set with username "([^"]*)"')
+def step_register(context, username):
+    context.driver.find_element_by_id("register_nickname").send_keys(username)
+
+@when('I set with password "([^"]*)"')
+def step_register(context, password):
+    context.driver.find_element_by_id("register_password").send_keys(password)
+
+@when('I set with code "([^"]*)"')
+def step_register(context, code):
+    context.driver.find_element_by_id("captcha_code").send_keys(code)
+
+@when('I click with registerbutton')
+def step_register(context):
+    context.driver.find_element_by_id("register-btn").click()
+
+@then('I expect that text "([^"]*)"')
+def setp_register(context,code_text):
+    text = context.driver.find_element_by_id("captcha_code-error").text
+    assert code_text in text
